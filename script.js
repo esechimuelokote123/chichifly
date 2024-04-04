71e694df33337da021e8b24820fee61526a46c2b
esechimuelokote123 <165975005+esechimuelokote123@users.noreply.github.com>
2024-04-04 00:37:53 -0400

    Update and rename javascript to script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded',()=>{
-                          constbuttons=
-                          document.querySelectorAll('.beat-btn');
+                          const buttons=
+                        document.querySelectorAll('.beat-btn');
 buttons.forEach(button=>{
                 button.addEventListener('clik',()=>
                                         playSound(button.getAttribute('data-sound')));
 });
 });
 
-functionplaySound(soundFile){
-  constaudio=newAudio(sounds/${soundFile});audio.play();
+function playSound(soundFile){
+  const audio=newAudio(`sounds/${soundFile`});audio.play();
 }
                           
