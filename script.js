25c29fc038214969dc70b031805a1cbc13f83ca9
esechimuelokote123 <165975005+esechimuelokote123@users.noreply.github.com>
2024-04-12 15:58:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded',()=>{
-                          const buttons=
-                        document.querySelectorAll('.beat-btn');
-buttons.forEach(button=>{
-                button.addEventListener('clik',()=>
-                                        playSound(button.getAttribute('data-sound')));
-});
-});
 
-function playSound(soundFile){
-  const audio=newAudio(`sounds/${soundFile`});audio.play();
-}
-                          
